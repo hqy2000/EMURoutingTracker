49c1ac38ed297bc98938cc440c52bebd3b684bba
Qingyang Hu <me@hqy.moe>
2021-04-15 15:19:47 +0800

    Remove empty requests

--- chinaemu/Data/FavoritesData.swift
+++ chinaemu/Data/FavoritesData.swift
@@ -39,31 +39,37 @@ class FavoritesData: ObservableObject {
         }
         lastRefresh = Date()
         
-        moerailProvider.request(target: .trains(keywords: FavoritesProvider.shared.favoriteTrains.map({ favorite in
-            return favorite.name
-        })), type: [EMU].self) { (result) in
-            self.favoriteTrains = result
-            for (index, emu) in result.enumerated() {
-                TimetableProvider.shared.get(forTrain: emu.singleTrain, onDate: emu.date) { (timetable) in
-                    self.favoriteTrains[index].timetable = timetable
+        if !FavoritesProvider.shared.favoriteTrains.isEmpty {
+            moerailProvider.request(target: .trains(keywords: FavoritesProvider.shared.favoriteTrains.map({ favorite in
+                return favorite.name
+            })), type: [EMU].self) { (result) in
+                self.favoriteTrains = result
+                for (index, emu) in result.enumerated() {
+                    TimetableProvider.shared.get(forTrain: emu.singleTrain, onDate: emu.date) { (timetable) in
+                        self.favoriteTrains[index].timetable = timetable
+                    }
                 }
+            } failure: { (error) in
+                print(error)
             }
-        } failure: { (error) in
-            print(error)
+        }
+       
+        
+        if !FavoritesProvider.shared.favoriteEMUs.isEmpty {
+            moerailProvider.request(target: .emus(keywords: FavoritesProvider.shared.favoriteEMUs.map({ favorite in
+                return favorite.name
+            })), type: [EMU].self) { (result) in
+                self.favoriteEMUs = result
+                for (index, emu) in result.enumerated() {
+                    TimetableProvider.shared.get(forTrain: emu.singleTrain, onDate: emu.date) { (timetable) in
+                        self.favoriteEMUs[index].timetable = timetable
+                    }
+                }
+            } failure: { (error) in
+                print(error)
+            }
         }
         
-        moerailProvider.request(target: .emus(keywords: FavoritesProvider.shared.favoriteEMUs.map({ favorite in
-            return favorite.name
-        })), type: [EMU].self) { (result) in
-            self.favoriteEMUs = result
-            for (index, emu) in result.enumerated() {
-                TimetableProvider.shared.get(forTrain: emu.singleTrain, onDate: emu.date) { (timetable) in
-                    self.favoriteEMUs[index].timetable = timetable
-                }
-            }
-        } failure: { (error) in
-            print(error)
-        }
         
     }
 }
