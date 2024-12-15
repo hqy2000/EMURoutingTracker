3ffcdc4c283ac63ec112540c5c1fa301a7ceb40d
Qingyang Hu <12138874+hqy2000@users.noreply.github.com>
2024-12-14 19:26:52 -0800

    Update assets

--- chinaemuFramework/Data/CRData.swift
+++ chinaemuFramework/Data/CRData.swift
@@ -16,9 +16,7 @@ class CRData: ObservableObject {
     @Published var emus: [EMU] = []
     
     public func getLeftTickets(from: String, to: String, date: Date) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        crProvider.request(target: .leftTicketPrice(from: from, to: to, date: dateFormatter.string(from: date)), type: CRResponse<[LeftTicketInfo]>.self) { (result) in
+        crProvider.request(target: .leftTicketPrice(from: from, to: to, date: DateFormatter.standard.string(from: date)), type: CRResponse<[LeftTicketInfo]>.self) { (result) in
             self.leftTickets = result.data
             self.moeRailProvider.request(target: .trains(keywords: result.data.map({ $0.leftTicket.trainNo })), type: [EMU].self, success: { (emus) in
                 self.emus = emus
