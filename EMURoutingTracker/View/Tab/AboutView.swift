0a53f38f8a72c08ab9be93715e66be1d45148a1a
Qingyang Hu <12138874+hqy2000@users.noreply.github.com>
2025-05-11 19:33:50 -0700

    Bump

--- EMURoutingTracker/View/Tab/AboutView.swift
+++ EMURoutingTracker/View/Tab/AboutView.swift
@@ -43,7 +43,10 @@ struct AboutView: View {
                 })
                 {
                     HStack {
-                        Text("源代码").font(.footnote)
+                        VStack(alignment: .leading, spacing: 2) {
+                            Text("源代码").font(.footnote).foregroundColor(.blue)
+                            Text("欢迎在 GitHub 上加星支持 ⭐️").font(.caption2).foregroundColor(.secondary)
+                        }
                         Spacer()
                         Text("GitHub").font(.footnote)
                     }
